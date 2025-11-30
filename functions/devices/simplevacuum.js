4889f3474fb8953c31c2d19c60c4c4720bce61bc
Michael Krug <michi.krug@gmail.com>
2025-11-30 13:31:52 +0100

    Remove obsolete simplevacuum
    Signed-off-by: Michael Krug <michi.krug@gmail.com>

--- functions/devices/simplevacuum.js
+++
@@ -1,9 +0,0 @@
-const StartStopSwitch = require('./startstopswitch.js');
-
-class SimpleVacuum extends StartStopSwitch {
-  static get type() {
-    return 'action.devices.types.VACUUM';
-  }
-}
-
-module.exports = SimpleVacuum;
